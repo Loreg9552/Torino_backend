0472a81de1fa15ca83b5fa3e903d657b6b289a8b
Loreg9552 <loreg9552@gmail.com>
2026-04-21 16:25:27 +0200

    modificato file di insert sql

--- user_api/user_api/src/main/resources/sql/db_01.sql
+++ user_api/user_api/src/main/resources/sql/db_01.sql
@@ -1,18 +1,10 @@
 --create default values for table "users"
-INSERT INTO users_db.users VALUES (1,'teacher','Miriam');
-INSERT INTO users_db.users VALUES (2,'none at the moment','Lorenzo');
-INSERT INTO users_db.users VALUES (3,'none at the moment','Paolo');
-INSERT INTO users_db.users VALUES (4,'insegnante','Elena');
-INSERT INTO users_db.users VALUES (6,'filosofo','Luciano');
-INSERT INTO users_db.users VALUES (7,'insegnante di danza','Carmelo');
-INSERT INTO users_db.users VALUES (8,'insegnante di danza','Pablo');
-INSERT INTO users_db.users VALUES (9,'sub','Coral');
-INSERT INTO users_db.users VALUES (10,'none at the moment','nessun utente');
+INSERT INTO `users` VALUES (1,'insegnante di italiano','Miriam'),(2,'disoccupato','Lorenzo'),(3,'disoccupato','Paolo'),
+                           (4,'insegnante','Elena'),(6,'filosofo','Luciano'),(7,'insegnante di danza','Carmelo'),
+                           (8,'insegnante di danza','Pablo'),(10,'gestore applicativo Torino','admin'),(11,'stilista','Paolo'),
+                           (12,'veterinaria','Paola'),(14,'presentatore','Carlo');
+--create default values for table "Immobili"
+INSERT INTO `immobili` VALUES (1,'50','120','via degli alberi',10),(2,'50','120','via degli alberi',10);
 --create default values for table "books"
-INSERT INTO users_db.books VALUES (1,1,'odissea');
-INSERT INTO users_db.books VALUES (2,2,'illiade');
-INSERT INTO users_db.books VALUES (3,10,'sparta');
-INSERT INTO users_db.books VALUES (4,10,'atene e la sua storia');
---create default values for table "immobili"
-INSERT INTO users_db.immobili VALUES (1,'50','120','via degli alberi',10);
-INSERT INTO users_db.immobili VALUES (2,'50','120','via degli alberi',10);
+INSERT INTO `books` VALUES (1,1,'odissea'),(2,2,'illiade'),(3,3,'atene e la sua storia'),(4,10,'odissea'),(5,10,'Roma'),
+                           (6,10,'Milano'),(7,10,'le storie dei cento acri');
